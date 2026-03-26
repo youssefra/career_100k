9710fa733a25f051c32fc03fbfd775f94f7d47a1
Youssef <ye2292003@gmail.com>
2026-03-26 08:30:31 +0200

    Day 11: Automation and Location Mastery Complete

---
+++ day_11/setup_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Starting Server Setup..."
+mkdir -p web_files
+touch web_files/index.html
+echo "<h1>Welcome to my 100k Career</h1>" > web_files/index.html
+echo "Setup Complete!"
