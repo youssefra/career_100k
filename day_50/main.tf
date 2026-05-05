c7a7253db191dbad20debe60e6d62c547bae57c5
Youssef <ye2292003@gmail.com>
2026-05-05 17:08:25 +0300

    Day 50: Fixed shell interpolation in local-exec provisioner

---
+++ day_50/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+  endpoints { s3 = "http://localhost:4566" }
+}
+
+resource "aws_s3_bucket" "automation_test" {
+  bucket = "milestone-50-bucket"
+
+  # THIS IS THE PROVISIONER
+  provisioner "local-exec" {
+    command = "echo 'Bucket ${self.bucket} was created at' $(date) > success.txt"
+  }
+}
